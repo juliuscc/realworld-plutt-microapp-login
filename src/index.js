084d9b61e20e68caacccb5e2fe7de9328ce1c23b
juliuscc <julius.cc@hotmail.com>
2020-05-22 12:00:16 +0200

    feat: allow soft navigation

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 
 import LoginForm from "./LoginForm";
 
-const Login = () => {
+const Login = ({ routeTo }) => {
+  const internalRoute = (route) => (e) => {
+    event.preventDefault();
+    routeTo(route);
+  };
+
   return (
     <div className="auth-page">
       <div className="container page">
@@ -10,7 +15,9 @@ const Login = () => {
           <div className="col-md-6 offset-md-3 col-xs-12">
             <h1 className="text-xs-center">Sign in</h1>
             <p className="text-xs-center">
-              <a href="/user/register">Need an account?</a>
+              <a href="/user/register" onClick={internalRoute("user/register")}>
+                Need an account?
+              </a>
             </p>
             <LoginForm />
           </div>
